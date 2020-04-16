a7ee5b3f366566e1c56a95b1665c6bdb00e9f9c3
pranav <pranav@gmail.com>
2020-04-16 18:46:14 +0530

    latest

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -756,7 +756,7 @@ $settings['entity_update_backup'] = TRUE;
    include $app_root . '/' . $site_path . '/settings.local.php';
  }
 $databases['default']['default'] = array (
-  'database' => 'careers_devnew',
+  'database' => 'careers_devnew1',
   'username' => 'root',
   'password' => '',
   'prefix' => '',
