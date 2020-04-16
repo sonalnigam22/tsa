0693e834bb89cfe1d5d54fe72f24b947dd13a7b5
sonalnigam22 <58845752+sonalnigam22@users.noreply.github.com>
2020-04-16 19:13:09 +0530

    Update settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -756,7 +756,7 @@ $settings['entity_update_backup'] = TRUE;
    include $app_root . '/' . $site_path . '/settings.local.php';
  }
 $databases['default']['default'] = array (
-  'database' => 'careers_devnew3',
+  'database' => 'careers_dev',
   'username' => 'root',
   'password' => '',
   'prefix' => '',
